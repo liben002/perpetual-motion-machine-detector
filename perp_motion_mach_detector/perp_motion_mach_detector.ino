ab2e9723c059f742fd6a264211911d93db3bc43f
liben002 <pharmashawarma@gmail.com>
2022-01-05 12:58:33 -0500

    Added beeping during scan, got rid of flickering issue on LCD

--- perp_motion_mach_detector/perp_motion_mach_detector.ino
+++ perp_motion_mach_detector/perp_motion_mach_detector.ino
@@ -18,6 +18,10 @@ const int scan_button_pin = 2;
 int scan_button_state;
 bool scan_ready;
 
+// piezo buzzer
+const int tone_freq = 440;
+const int buzzer_pin = 8;
+
 void setup() {
   // put your setup code here, to run once:
   lcd.init();
@@ -26,8 +30,11 @@ void setup() {
   lcd.print("Initializing...");
 
   stage = 0;
+
+  // setup buzzer
+  pinMode(buzzer_pin, OUTPUT);
   
-  // Setup scan button
+  // setup scan button
   pinMode(scan_button_pin, INPUT);
   scan_button_state = 0;
   scan_ready = true;
@@ -41,34 +48,64 @@ void setup() {
 }
 
 void loop() {
-  scan_button_state = digitalRead(scan_button_pin);
-  // put your main code here, to run repeatedly:
-  if (stage == 0) {
-    lcd.clear();
+  lcd.clear();
+  
+  while(digitalRead(scan_button_pin) != HIGH) {
     lcd.setCursor(0,0);
     lcd.print(base_message_top);
     lcd.setCursor(0,1);
     lcd.print(base_message_bottom);
-  } else if (stage == 1) {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(scan_message);
-    delay(2000);
-    stage = 2;
-  } else {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(result_message_top);
-    lcd.setCursor(0,1);
-    lcd.print(result_message_bottom);
-    scan_ready = true;
-    stage = 0;
-    delay(5000);
   }
 
-  if (scan_button_state == HIGH && scan_ready) {
-    stage = 1;
-    scan_ready = false;
-  }
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(scan_message);
+  beep(tone_freq, 200);
+  beep(tone_freq, 200);
+  beep(tone_freq, 1000);
+  stage = 2;
+
+  lcd.setCursor(0,0);
+  lcd.print(result_message_top);
+  lcd.setCursor(0,1);
+  lcd.print(result_message_bottom);
+
+  delay(5000);
+
   
+//  scan_button_state = digitalRead(scan_button_pin);
+//  // put your main code here, to run repeatedly:
+//  if (stage == 0) {
+//    lcd.setCursor(0,0);
+//    lcd.print(base_message_top);
+//    lcd.setCursor(0,1);
+//    lcd.print(base_message_bottom);
+//  } else if (stage == 1) {
+//    lcd.setCursor(0,0);
+//    lcd.print(scan_message);
+//    beep(tone_freq, 200);
+//    beep(tone_freq, 200);
+//    beep(tone_freq, 1000);
+//    stage = 2;
+//  } else {
+//    lcd.setCursor(0,0);
+//    lcd.print(result_message_top);
+//    lcd.setCursor(0,1);
+//    lcd.print(result_message_bottom);
+//    scan_ready = true;
+//    stage = 0;
+//    delay(5000);
+//  }
+//
+//  if (scan_button_state == HIGH && scan_ready) {
+//    stage = 1;
+//    scan_ready = false;
+//  }
+}
+
+void beep(int note, int duration) {
+  // play tone on buzzer_pin
+  tone(buzzer_pin, 440, duration);
+  delay(duration);
+  noTone(buzzer_pin);
 }
